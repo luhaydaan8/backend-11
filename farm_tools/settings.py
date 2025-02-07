c9d2a4553e90d9e5c4a1fb32cefd75246ad8056e
luhaydaan8 <harithghassany@gmail.com>
2025-02-07 14:00:31 +0300

    Update settings.py

--- farm_tools/settings.py
+++ farm_tools/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',  
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
