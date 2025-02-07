a78496eb6ca0a0f8d3951e12af6ff2d08132a0df
luhaydaan8 <harithghassany@gmail.com>
2025-02-07 13:53:08 +0300

    Update settings.py

--- farm_tools/settings.py
+++ farm_tools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u2cm*e8dtl6v1at5xb)#175y$bzu!a*07!psq!7@j=(g&icy-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
